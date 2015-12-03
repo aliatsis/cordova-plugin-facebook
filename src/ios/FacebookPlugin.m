d5b2f407c3d1d5a51103aa7c3d6e02cfdb40a595
Anthony Liatsis <anthony.liatsis@gmail.com>
2015-12-03 03:42:45 -0800

    finish ios

--- src/ios/FacebookPlugin.m
+++ src/ios/FacebookPlugin.m
@@ -4,37 +4,46 @@
 
 @implementation FacebookPlugin : CDVPlugin
 
+NSDictionary *appEventNameConstByString = nil;
+NSDictionary *appEventParameterNameConstByString = nil;
+
 - (void)pluginInitialize
 {
     NSLog(@"[FacebookPlugin] plugin initialized");
 
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(finishLaunching:) name:UIApplicationDidFinishLaunchingNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil]; 
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURLNotification:) name:CDVPluginHandleOpenURLNotification object:nil]; 
+    #ifdef DEBUG
+        //empty
+    #else
+        [self setAppEventConstByString];
+    
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(finishLaunching:) name:UIApplicationDidFinishLaunchingNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURLNotification:) name:CDVPluginHandleOpenURLNotification object:nil];
+    #endif
 }
 
 - (void)finishLaunching:(NSNotification *)notification
 {
     #ifdef DEBUG
         [FBSDKSettings enableLoggingBehavior:FBSDKLoggingBehaviorAppEvents];
-   #else
-        UIApplication *application = [UIApplication sharedApplication];
-        NSDictionary *launchOptions = [notification userInfo];
-
-        [[FBSDKApplicationDelegate sharedInstance] application:application
-            didFinishLaunchingWithOptions:launchOptions];
     #endif
+    
+    UIApplication *application = [UIApplication sharedApplication];
+    NSDictionary *launchOptions = [notification userInfo];
+    
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (void)handleOpenURLNotification:(NSNotification*)notification
 {
     UIApplication *application = [UIApplication sharedApplication];
 
-//     [[FBSDKApplicationDelegate sharedInstance] application:application
-//         openURL:[notification object]
-//         sourceApplication:sourceApplication
-//         annotation:annotation
-//       ];
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+        openURL:[notification object]
+        sourceApplication:nil
+        annotation:nil
+    ];
 }
 
 - (void)onAppDidBecomeActive:(NSNotification*)notification
@@ -53,17 +62,13 @@
         currency = @"USD";
     }
 
-    if (parameters == (id)[NSNull null]) {
-        parameters = nil;
-    }
-
     if (accessToken == (id)[NSNull null]) {
         accessToken = nil;
     }
     
     [FBSDKAppEvents logPurchase:[purchaseAmount doubleValue]
               currency:currency
-              parameters:parameters
+              parameters:[self getParametersWithConstKeys:parameters]
               accessToken:accessToken ];
 }
 
@@ -78,18 +83,73 @@
         valueToSum = nil;
     }
 
-    if (parameters == (id)[NSNull null]) {
-        parameters = nil;
-    }
-
     if (accessToken == (id)[NSNull null]) {
         accessToken = nil;
     }
     
-    [FBSDKAppEvents logEvent:eventName
+    [FBSDKAppEvents logEvent:appEventNameConstByString[eventName]
               valueToSum:valueToSum
-              parameters:parameters
+              parameters:[self getParametersWithConstKeys:parameters]
               accessToken:accessToken ];
+}
+
+- (NSDictionary*)getParametersWithConstKeys:(NSDictionary*)parameters
+{
+    if (parameters == (id)[NSNull null]) {
+        return nil;
+    }
+    
+    NSMutableDictionary *result = [[NSMutableDictionary alloc] init];
+    
+    for(id key in parameters) {
+        NSString* constValue = [appEventParameterNameConstByString objectForKey:key];
+        NSString* newKey = constValue == nil ? key : constValue;
+        id value = [parameters objectForKey:key];
+        
+        if ([value isEqual:@(YES)]) {
+            value = FBSDKAppEventParameterValueYes;
+        } else if ([value isEqual:@(NO)]) {
+            value = FBSDKAppEventParameterValueYes;
+        }
+        
+        [result setObject:value forKey:newKey];
+    }
+    
+    return result;
+}
+
+- (void)setAppEventConstByString
+{
+    appEventNameConstByString =
+    @{
+      @"FBSDKAppEventNameAchievedLevel": FBSDKAppEventNameAchievedLevel,
+      @"FBSDKAppEventNameAddedPaymentInfo": FBSDKAppEventNameAddedPaymentInfo,
+      @"FBSDKAppEventNameAddedToCart": FBSDKAppEventNameAddedToCart,
+      @"FBSDKAppEventNameAddedToWishlist": FBSDKAppEventNameAddedToWishlist,
+      @"FBSDKAppEventNameCompletedRegistration": FBSDKAppEventNameCompletedRegistration,
+      @"FBSDKAppEventNameCompletedTutorial": FBSDKAppEventNameCompletedTutorial,
+      @"FBSDKAppEventNameInitiatedCheckout": FBSDKAppEventNameInitiatedCheckout,
+      @"FBSDKAppEventNameRated": FBSDKAppEventNameRated,
+      @"FBSDKAppEventNameSearched": FBSDKAppEventNameSearched,
+      @"FBSDKAppEventNameSpentCredits": FBSDKAppEventNameSpentCredits,
+      @"FBSDKAppEventNameUnlockedAchievement": FBSDKAppEventNameUnlockedAchievement,
+      @"FBSDKAppEventNameViewedContent": FBSDKAppEventNameViewedContent
+    };
+    
+    appEventParameterNameConstByString =
+    @{
+      @"FBSDKAppEventParameterNameContentID": FBSDKAppEventParameterNameContentID,
+      @"FBSDKAppEventParameterNameContentType": FBSDKAppEventParameterNameContentType,
+      @"FBSDKAppEventParameterNameCurrency": FBSDKAppEventParameterNameCurrency,
+      @"FBSDKAppEventParameterNameDescription": FBSDKAppEventParameterNameDescription,
+      @"FBSDKAppEventParameterNameLevel": FBSDKAppEventParameterNameLevel,
+      @"FBSDKAppEventParameterNameMaxRatingValue": FBSDKAppEventParameterNameMaxRatingValue,
+      @"FBSDKAppEventParameterNameNumItems": FBSDKAppEventParameterNameNumItems,
+      @"FBSDKAppEventParameterNamePaymentInfoAvailable": FBSDKAppEventParameterNamePaymentInfoAvailable,
+      @"FBSDKAppEventParameterNameRegistrationMethod": FBSDKAppEventParameterNameRegistrationMethod,
+      @"FBSDKAppEventParameterNameSearchString": FBSDKAppEventParameterNameSearchString,
+      @"FBSDKAppEventParameterNameSuccess": FBSDKAppEventParameterNameSuccess
+    };
 }
 
 @end
